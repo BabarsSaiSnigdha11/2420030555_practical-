d2484bf5fd2c94ff4590fd09ae56770095d309f5
BabarsSaiSnigdha11 <snigdhababars@gmail.com>
2025-08-02 09:21:38 +0530

    Add files via upload

---
+++ calender.java
@@ -0,0 +1,30 @@
+package MyProject_Day1;
+import java.util.*;
+
+public class calender {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Calendar c=Calendar.getInstance();
+		Date d=c.getTime();
+		System.out.println(d);
+		c.set(2023,c.SEPTEMBER,13);
+		int year=c.getWeekYear();
+		System.out.println(year);
+		int h=c.getFirstDayOfWeek();
+		System.out.println(h);
+		System.out.println("Day of week :" +c.get(Calendar.DAY_OF_WEEK));
+		System.out.println("Day of year :" +c.get(Calendar.DAY_OF_YEAR));
+		System.out.println("Week in Month :" + c.get(Calendar.WEEK_OF_MONTH));
+		System.out.println("Week in Year :" +c.get(Calendar.WEEK_OF_YEAR));
+		System.out.println("Day of Week in Month:" +c.get(Calendar.DAY_OF_WEEK_IN_MONTH));
+		System.out.println("Hour :" +c.get(Calendar.HOUR));
+		System.out.println("Minute :" +c.get(Calendar.MINUTE));
+		System.out.println("Second :" +c.get(Calendar.SECOND));
+		System.out.println("AM or PM :" +c.get(Calendar.AM_PM));
+		System.out.println("Hour (24-hour clock) :" +c.get(Calendar.HOUR_OF_DAY));
+		
+
+	}
+
+}
